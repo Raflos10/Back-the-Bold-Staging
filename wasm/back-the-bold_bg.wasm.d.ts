52cc1ed2ef23a9c305c8ce53d08217c2e152b2c4
Raflos10 <Raflos10@users.noreply.github.com>
2025-05-10 09:02:52 +0000

    deploy: Raflos10/Back-the-Bold@49ccb91ba9f9596e8e60a5530044d8d9cea11cc4

---
+++ wasm/back-the-bold_bg.wasm.d.ts
@@ -0,0 +1,19 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const main: (a: number, b: number) => number;
+export const __wbg_jsowner_free: (a: number, b: number) => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_exn_store: (a: number) => void;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_export_4: WebAssembly.Table;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __externref_drop_slice: (a: number, b: number) => void;
+export const __wbindgen_export_7: WebAssembly.Table;
+export const _ZN132__LT_dyn_u20_core__ops__function__FnMut_LT__LP__RP__GT__u2b_Output_u20__u3d__u20_R_u20_as_u20_wasm_bindgen__closure__WasmClosure_GT_8describe6invoke17h147c41e7167d08a3E: (a: number, b: number) => void;
+export const _ZN132__LT_dyn_u20_core__ops__function__FnMut_LT__LP__RP__GT__u2b_Output_u20__u3d__u20_R_u20_as_u20_wasm_bindgen__closure__WasmClosure_GT_8describe6invoke17ha5856002ac70b0f5E: (a: number, b: number) => void;
+export const closure2460_externref_shim: (a: number, b: number, c: any) => void;
+export const closure2466_externref_shim: (a: number, b: number, c: any) => void;
+export const closure2783_externref_shim: (a: number, b: number, c: any) => void;
+export const __wbindgen_start: () => void;
